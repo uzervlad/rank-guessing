793ead0a2ba25bf1e03034c9790a6eb73da2f869
Octo <vladik0701@gmail.com>
2025-08-26 03:36:13 +0300

    add analysis result to admin panel

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import Button from '$lib/components/Button.svelte';
+  import CircleAlert from '@lucide/svelte/icons/circle-alert';
+  import CircleCheck from '@lucide/svelte/icons/circle-check';
+  import CircleX from '@lucide/svelte/icons/circle-x';
 
   const { data } = $props();
 </script>
@@ -24,13 +27,18 @@
         <th>ID</th>
         <th>User</th>
         <th>Beatmap</th>
+        <th></th>
+        <th></th>
+        <th></th>
         <th>Actions</th>
       </tr>
     </thead>
     <tbody>
       {#each data.requests as {requests, beatmaps}}
       <tr>
-        <td>{requests.id}</td>
+        <td>
+          {requests.id}
+        </td>
         <td>
           <a href={`https://osu.ppy.sh/u/${requests.player_id}`}>
             {requests.player_id}
@@ -40,6 +48,31 @@
           <a href={`https://osu.ppy.sh/b/${beatmaps.id}`}>
             {beatmaps.artist} - {beatmaps.title} [{beatmaps.version}]
           </a>
+        </td>
+        <td title={requests.client_state}>
+          {#if requests.client_state === 'lazer-mods'}
+            <CircleAlert color="yellow" />
+          {:else}
+            <CircleCheck color="#32d232" />
+          {/if}
+        </td>
+        <td title={requests.user_state}>
+          {#if requests.user_state === 'same_user'}
+            <CircleCheck color="#32d232" />
+          {:else if requests.user_state === 'other_user'}
+            <CircleX color="red" />
+          {:else}
+            <CircleAlert color="yellow" />
+          {/if}
+        </td>
+        <td title={requests.online_state}>
+          {#if requests.online_state === 'available'}
+            <CircleCheck color="#32d232" />
+          {:else if requests.online_state === 'unavailable'}
+            <CircleAlert color="yellow" />
+          {:else}
+            <CircleX color="red" />
+          {/if}
         </td>
         <td>
           <Button variant='danger'>
