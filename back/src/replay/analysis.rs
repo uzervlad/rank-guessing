3cec9724e3da882b57801fbb17dddb679784e4d9
Octo <vladik0701@gmail.com>
2025-11-06 22:01:00 +0300

    move database enum implementation to a macro

--- back/src/replay/analysis.rs
+++ back/src/replay/analysis.rs
@@ -6,187 +6,90 @@ use sqlx::{Decode, Encode, Sqlite, Type, sqlite::SqliteTypeInfo};
 
 use crate::replay::Replay;
 
-#[derive(Clone, Copy, Debug, Serialize)]
-#[serde(rename_all = "kebab-case")]
-pub enum ClientState {
-	Stable,
-	Lazer,
-	LazerMods,
-}
+macro_rules! sqlite_enum {
+	($name:ident, $($key:ident => $value:literal),+) => {
+		#[derive(Clone, Copy, Debug, Serialize)]
+		#[serde(rename_all = "kebab-case")]
+		pub enum $name {
+			$(
+				$key,
+			)*
+		}
 
-impl Display for ClientState {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(
-			f,
-			"{}",
-			match self {
-				Self::Stable => "stable",
-				Self::Lazer => "lazer",
-				Self::LazerMods => "lazer-mods",
+		impl Display for $name {
+			fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+				write!(
+					f,
+					"{}",
+					match self {
+						$(
+							Self::$key => $value,
+						)*
+					}
+				)
 			}
-		)
-	}
-}
+		}
 
-impl TryFrom<String> for ClientState {
-	type Error = ();
+		impl TryFrom<String> for $name {
+			type Error = ();
 
-	fn try_from(value: String) -> Result<Self, Self::Error> {
-		match value.as_str() {
-			"stable" => Ok(Self::Stable),
-			"lazer" => Ok(Self::Lazer),
-			"lazer-mods" => Ok(Self::LazerMods),
-			_ => Err(()),
+			fn try_from(value: String) -> Result<Self, Self::Error> {
+				match value.as_str() {
+					$(
+						$value => Ok(Self::$key),
+					)*
+					_ => Err(()),
+				}
+			}
+		}
+
+		impl Type<Sqlite> for $name {
+			fn type_info() -> SqliteTypeInfo {
+				<String as Type<Sqlite>>::type_info()
+			}
+		}
+
+		impl<'q> Encode<'q, Sqlite> for $name {
+			fn encode_by_ref(
+				&self,
+				buf: &mut <Sqlite as sqlx::Database>::ArgumentBuffer<'q>,
+			) -> Result<sqlx::encode::IsNull, sqlx::error::BoxDynError> {
+				<String as Encode<'q, Sqlite>>::encode(self.to_string(), buf)
+			}
+		}
+
+		impl<'r> Decode<'r, Sqlite> for $name {
+			fn decode(
+				value: <Sqlite as sqlx::Database>::ValueRef<'r>,
+			) -> Result<Self, sqlx::error::BoxDynError> {
+				let value = <String as Decode<'r, Sqlite>>::decode(value)?;
+
+				Self::try_from(value.clone())
+					.map_err(|_| format!("invalid value `{}` for enum {}", value, stringify!($name)).into())
+			}
 		}
 	}
 }
 
-impl Type<Sqlite> for ClientState {
-	fn type_info() -> SqliteTypeInfo {
-		<String as Type<Sqlite>>::type_info()
-	}
+sqlite_enum! {
+	ClientState,
+	Stable => "stable",
+	Lazer => "lazer",
+	LazerMods => "lazer-mods"
 }
 
-impl<'q> Encode<'q, Sqlite> for ClientState {
-	fn encode_by_ref(
-		&self,
-		buf: &mut <Sqlite as sqlx::Database>::ArgumentBuffer<'q>,
-	) -> Result<sqlx::encode::IsNull, sqlx::error::BoxDynError> {
-		<String as Encode<'q, Sqlite>>::encode(self.to_string(), buf)
-	}
+sqlite_enum! {
+	OnlineState,
+	Available => "available",
+	Unavailable => "unavailable",
+	NotPresent => "not-present"
 }
 
-impl<'r> Decode<'r, Sqlite> for ClientState {
-	fn decode(
-		value: <Sqlite as sqlx::Database>::ValueRef<'r>,
-	) -> Result<Self, sqlx::error::BoxDynError> {
-		let value = <String as Decode<'r, Sqlite>>::decode(value)?;
-
-		Self::try_from(value.clone())
-			.map_err(|_| format!("invalid value `{value}` for enum ClientState").into())
-	}
-}
-
-#[derive(Clone, Copy, Debug, Serialize)]
-#[serde(rename_all = "kebab-case")]
-pub enum OnlineState {
-	Available,
-	Unavailable,
-	NotPresent,
-}
-
-impl Display for OnlineState {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(
-			f,
-			"{}",
-			match self {
-				Self::Available => "available",
-				Self::Unavailable => "unavailable",
-				Self::NotPresent => "not-present",
-			}
-		)
-	}
-}
-
-impl TryFrom<String> for OnlineState {
-	type Error = ();
-
-	fn try_from(value: String) -> Result<Self, Self::Error> {
-		match value.as_str() {
-			"available" => Ok(Self::Available),
-			"unavailable" => Ok(Self::Unavailable),
-			"not-present" => Ok(Self::NotPresent),
-			_ => Err(()),
-		}
-	}
-}
-
-impl Type<Sqlite> for OnlineState {
-	fn type_info() -> SqliteTypeInfo {
-		<String as Type<Sqlite>>::type_info()
-	}
-}
-
-impl<'q> Encode<'q, Sqlite> for OnlineState {
-	fn encode_by_ref(
-		&self,
-		buf: &mut <Sqlite as sqlx::Database>::ArgumentBuffer<'q>,
-	) -> Result<sqlx::encode::IsNull, sqlx::error::BoxDynError> {
-		<String as Encode<'q, Sqlite>>::encode(self.to_string(), buf)
-	}
-}
-
-impl<'r> Decode<'r, Sqlite> for OnlineState {
-	fn decode(
-		value: <Sqlite as sqlx::Database>::ValueRef<'r>,
-	) -> Result<Self, sqlx::error::BoxDynError> {
-		let value = <String as Decode<'r, Sqlite>>::decode(value)?;
-
-		Self::try_from(value.clone())
-			.map_err(|_| format!("invalid value `{value}` for enum ClientState").into())
-	}
-}
-
-#[derive(Clone, Copy, Debug, Serialize)]
-#[serde(rename_all = "kebab-case")]
-pub enum UserState {
-	SameUser,
-	OtherUser,
-	NotPresent,
-}
-
-impl Display for UserState {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		write!(
-			f,
-			"{}",
-			match self {
-				Self::SameUser => "same-user",
-				Self::OtherUser => "other-user",
-				Self::NotPresent => "not-present",
-			}
-		)
-	}
-}
-
-impl TryFrom<String> for UserState {
-	type Error = ();
-
-	fn try_from(value: String) -> Result<Self, Self::Error> {
-		match value.as_str() {
-			"same-user" => Ok(Self::SameUser),
-			"other-user" => Ok(Self::OtherUser),
-			"not-present" => Ok(Self::NotPresent),
-			_ => Err(()),
-		}
-	}
-}
-
-impl Type<Sqlite> for UserState {
-	fn type_info() -> SqliteTypeInfo {
-		<String as Type<Sqlite>>::type_info()
-	}
-}
-
-impl<'q> Encode<'q, Sqlite> for UserState {
-	fn encode_by_ref(
-		&self,
-		buf: &mut <Sqlite as sqlx::Database>::ArgumentBuffer<'q>,
-	) -> Result<sqlx::encode::IsNull, sqlx::error::BoxDynError> {
-		<String as Encode<'q, Sqlite>>::encode(self.to_string(), buf)
-	}
-}
-
-impl<'r> Decode<'r, Sqlite> for UserState {
-	fn decode(
-		value: <Sqlite as sqlx::Database>::ValueRef<'r>,
-	) -> Result<Self, sqlx::error::BoxDynError> {
-		let value = <String as Decode<'r, Sqlite>>::decode(value)?;
-
-		Self::try_from(value.clone())
-			.map_err(|_| format!("invalid value `{value}` for enum ClientState").into())
-	}
+sqlite_enum! {
+	UserState,
+	SameUser => "same-user",
+	OtherUser => "other-user",
+	NotPresent => "not-present"
 }
 
 pub trait ReplayAnalysisTrait {
